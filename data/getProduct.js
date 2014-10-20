abf65515bcab8d1fe099e7321f9db618fe28641b
Zordius Chen <zordius@yahoo-inc.com>
2014-10-20 16:27:34 +0800

    more sample

--- data/getProduct.js
+++ data/getProduct.js
@@ -4,9 +4,19 @@
 var task = require('subtask');
 
 module.exports = function (product_id) {
+    // example: input validation
+    if (!product_id) {
+        return task();
+    }
+
     return task(function (cb) {
         // example: async call to get product
         setTimeout(function() {
+            // example: data validation
+            if (Math.floor(product_id) != product_id) {
+                return cb();
+            }
+
             cb({
                 title: 'this is sample product title (id=' + product_id + ')',
                 description: 'sample product description',
