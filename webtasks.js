97c95a9c6e47b8ca55895aade1695380a6c8f6e0
Zordius Chen <zordius@yahoo-inc.com>
2014-10-20 14:37:15 +0800

    move to lib

--- webtasks.js
+++ webtasks.js
@@ -3,37 +3,17 @@
 
 var express = require('express'),
     exphbs = require('express-handlebars'),
-    task = require('subtask'),
+    webtask = require('./lib/webtask'),
     app = module.exports = express();
 
 app.engine('.hbs', exphbs({defaultLayout: false, extname: '.hbs'}));
 app.set('view engine', '.hbs');
 
-app.webtask = function (name) {
-    var T = require(name);
-
-    app.use(function (req, res, next) {
-        T({
-            req: req,
-            res: res
-        }).execute(function (R) {
-            if (R && R.data) {
-                if (R.layout) {
-                    res.render(R.layout, R.data);
-                } else {
-                    res.json(R.data);
-                }
-            } else {
-                next();
-            }
-        });
-    });
-};
-
+app.webtask = webtask;
 
 /* istanbul ignore next */
 if (!module.parent) {
-    app.webtask('./pages/sample');
+    app.webtask(require('./pages/sample'));
     app.listen(3000);
     console.log('Webtasks started on port 3000');
 }
