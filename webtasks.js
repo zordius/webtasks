2af47f7d6728ec12a84d6c6f84092e5ead8be7fd
Zordius Chen <zordius@yahoo-inc.com>
2014-10-20 15:13:08 +0800

    fix locate()

--- webtasks.js
+++ webtasks.js
@@ -2,18 +2,15 @@
 'use strict';
 
 var express = require('express'),
-    exphbs = require('express-handlebars'),
     webtask = require('./lib/webtask'),
     app = module.exports = express();
 
-app.engine('.hbs', exphbs({defaultLayout: false, extname: '.hbs'}));
-app.set('view engine', '.hbs');
 
-app.webtask = webtask;
+webtask.init(app);
 
 /* istanbul ignore next */
 if (!module.parent) {
-    app.webtask(require('./pages/sample'));
+    app.webtask(app.page('sample'));
     app.listen(3000);
     console.log('Webtasks started on port 3000');
 }
