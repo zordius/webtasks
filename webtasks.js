be31d789fd30fca4720502f5a4362b7f41d29bd0
Zordius Chen <zordius@yahoo-inc.com>
2014-10-20 12:50:52 +0800

    hello world

--- webtasks.js
+++ webtasks.js
@@ -4,4 +4,12 @@
 var express = require('express'),
     app = module.exports = express();
 
-module.exports = {};
+app.get('/', function (req, res) {
+    res.send('OK! this is a page....');
+});
+
+/* istanbul ignore next */
+if (!module.parent) {
+  app.listen(3000);
+  console.log('Webtasks started on port 3000');
+}
