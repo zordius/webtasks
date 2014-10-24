aaa42ea552f9443fa8aacbf58defb3de2b52de2b
Zordius Chen <zordius@yahoo-inc.com>
2014-10-24 15:54:15 +0800

    fix sample middleware order

--- webtasks.js
+++ webtasks.js
@@ -9,9 +9,9 @@ webtask.init(app);
 
 /* istanbul ignore next */
 if (!module.parent) {
-    app.webtask('/', app.page('sample2'));
     app.webtask('/module/header', app.module('header'));
     app.webtask('/ajax/product', app.ajax('getProduct'));
+    app.webtask('/', app.page('sample2'));
     app.listen(3000);
     console.log('Webtasks started on port 3000');
 }
