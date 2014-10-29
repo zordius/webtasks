ff6063815343a5ffeca3bb109b678ec047b5a8ce
Zordius Chen <zordius@yahoo-inc.com>
2014-10-29 16:30:34 +0800

    now we handle onChange but no client side behaviors, should do client side tasks now

--- react/product.jsx
+++ react/product.jsx
@@ -6,13 +6,16 @@ Product = React.createClass({
     getInitialState: function () {
         return {selectedSpec: 0}
     },
+    handleSpecChange: function () {
+        this.setState({selectedSpec: event.target.value});
+    },
     render: function () {
         var specs = [],
             I;
 
         for (I=0;I<this.props.specs.length;I++) {
             specs.push(
-                <option id={this.props.specs[I].id}>{this.props.specs[I].title}</option>
+                <option value={this.props.specs[I].id} selected={I==this.state.selectedSpec}>{this.props.specs[I].title}</option>
             );
         }
 
@@ -21,7 +24,7 @@ Product = React.createClass({
  <h1>{this.props.title}</h1>
  <p>{this.props.description}</p>
  <span>Price: <i>$</i><b>{this.props.price}</b></span>
- <div>Pick a spec:<select>{specs}</select>
+ <div>Pick a spec:<select onChange={this.handleSpecChange}>{specs}</select>
   <LimitSelect max={this.props.specs[this.state.selectedSpec].limit} />
  </div>
 </div>
