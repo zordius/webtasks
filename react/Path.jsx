673ddd04355afc09b7666968c422d2b7a9daa879
Zordius Chen <zordius@yahoo-inc.com>
2014-10-30 13:35:12 +0800

    fix file name

---
+++ react/Path.jsx
@@ -0,0 +1,19 @@
+/** @jsx React.DOM */
+var React = require('react'),
+
+Path = React.createClass({
+    render: function () {
+        var paths = this.props.path.map(function (P) {
+            return (
+                <li><a href={'/cate/' + P.id}>{P.title}</a></li>
+            )
+        });
+        return (
+            <ul className="path">
+             {paths}
+            </ul>
+        );
+    }
+});
+
+module.exports = Path;
---
+++ react/Product.jsx
@@ -0,0 +1,35 @@
+/** @jsx React.DOM */
+var React = require('react'),
+    LimitSelect = require('./limitSelect.jsx'),
+
+Product = React.createClass({
+    getInitialState: function () {
+        return {selectedSpec: 1}
+    },
+    handleSpecChange: function () {
+        this.setState({selectedSpec: event.target.value});
+    },
+    render: function () {
+        var specs = [],
+            I;
+
+        for (I=0;I<this.props.specs.length;I++) {
+            specs.push(
+                <option value={this.props.specs[I].id} selected={I==this.state.selectedSpec}>{this.props.specs[I].title}</option>
+            );
+        }
+
+        return (
+<div>
+ <h1>{this.props.title}</h1>
+ <p>{this.props.description}</p>
+ <span>Price: <i>$</i><b>{this.props.price}</b></span>
+ <div>Pick a spec:<select onChange={this.handleSpecChange}>{specs}</select>
+  <LimitSelect max={this.props.specs[this.state.selectedSpec].limit} />
+ </div>
+</div>
+        );
+    }
+});
+
+module.exports = Product;
