5517ba01f034cfff8057b4059cf42e8e1628eb2b
Zordius Chen <zordius@yahoo-inc.com>
2014-10-30 13:30:42 +0800

    add dreact

--- page/sample3.js
+++ page/sample3.js
@@ -6,7 +6,7 @@ module.exports = function () {
         title: this.query('id').pipe(this.data('getProduct')).pick('title'),
         headerModule: this.data('getUser')().pipe(this.module('header')),
         pathModule: this.query('id').pipe(this.data('getProduct')).pick('category_id').pipe(this.data('getPath')).pipe(this.react('path')),
-        productModule: this.query('id').pipe(this.data('getProduct')).pipe(this.react('product')),
+        productModule: this.query('id').pipe(this.data('getProduct')).pipe(this.dreact('product')),
     }).transform(function (R) {
         return {
             title: R.title,
