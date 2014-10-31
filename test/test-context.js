5fc45bd4e918f45a6a06ffe26654fdbc06534d60
Zordius Chen <zordius@yahoo-inc.com>
2014-10-31 18:17:37 +0800

    add more test

--- test/test-context.js
+++ test/test-context.js
@@ -9,12 +9,39 @@ describe('context', function () {
         assert.equal(true, testlib.isSubtask(testlib.getMockContext().query));
         done();
     });
+
     it('.param should be a subtask', function (done) {
         assert.equal(true, testlib.isSubtask(testlib.getMockContext().param));
         done();
     });
+
     it('.cookie should be a subtask', function (done) {
         assert.equal(true, testlib.isSubtask(testlib.getMockContext().cookie));
+        done();
+    });
+
+    it('.data should be a subtask', function (done) {
+        assert.equal(true, testlib.isSubtask(testlib.getMockContext().data));
+        done();
+    });
+
+    it('.page should be a subtask', function (done) {
+        assert.equal(true, testlib.isSubtask(testlib.getMockContext().page));
+        done();
+    });
+
+    it('.react should be a subtask', function (done) {
+        assert.equal(true, testlib.isSubtask(testlib.getMockContext().react));
+        done();
+    });
+
+    it('.dreact should be a subtask', function (done) {
+        assert.equal(true, testlib.isSubtask(testlib.getMockContext().dreact));
+        done();
+    });
+
+    it('.task should be a subtask', function (done) {
+        assert.equal(true, testlib.isSubtask(testlib.getMockContext().task));
         done();
     });
 });
