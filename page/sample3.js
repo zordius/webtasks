75e837f6dd5871585b87c43eca4c3c307c69d1c2
Zordius Chen <zordius@yahoo-inc.com>
2014-11-12 16:20:41 +0800

    now handle error in reactRender() , and give more error detail when middleware done

--- page/sample3.js
+++ page/sample3.js
@@ -6,7 +6,7 @@ module.exports = function () {
         title: this.query('id').pipe(this.data('getProduct')).pick('title'),
         headerModule: this.data('getUser')().pipe(this.module('header')),
         pathModule: this.query('id').pipe(this.data('getProduct')).pick('category_id').pipe(this.data('getPath')).pipe(this.react('Path')),
-        productModule: this.query('id').pipe(this.data('getProduct')).pipe(this.dreact('Product')),
+//        productModule: this.query('id').pipe(this.data('getProduct')).pipe(this.dreact('Product')),
     }).transform(function (R) {
         return {
             title: R.title,
