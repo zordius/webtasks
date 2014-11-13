62b297bad28d07c163c7914cde0f24bd5903167d
Zordius Chen <zordius@yahoo-inc.com>
2014-11-13 09:56:51 +0800

    add more tests for render

--- test/test-render.js
+++ test/test-render.js
@@ -9,5 +9,22 @@ describe('render', function () {
         assert.equal(true, testlib.isSubtaskCreator(render));
         done();
     });
+
+    it('should create a task even when no input', function (done) {
+        assert.equal(true, testlib.isSubtask(render()));
+        done();
+    });
+
+    it('should create a task even when view not found', function (done) {
+        assert.equal(true, testlib.isSubtask(render({})));
+        done();
+    });
+
+    it('should render by a view', function (done) {
+        render({view: 'module_nothing'}).execute(function (R) {
+            assert.equal('<div>OK!</div>\n', R);
+            done();
+        });
+    });
 });
 
