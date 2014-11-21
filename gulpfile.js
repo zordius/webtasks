05100f36d7b2506e4f1af3bf572551856b1ac346
Zordius Chen <zordius@yahoo-inc.com>
2014-11-21 22:52:17 +0800

    try to move to faster watchify

--- gulpfile.js
+++ gulpfile.js
@@ -1,52 +1,85 @@
 var gulp = require('gulp'),
-    browserify = require('gulp-browserify'),
+    gutil = require('gulp-util'),
+    browserify = require('browserify'),
+    watchify = require('watchify'),
+    source = require('vinyl-source-stream'),
     sourcemaps = require('gulp-sourcemaps'),
     concat = require('gulp-concat'),
     nodemon = require('gulp-nodemon'),
+    react = require('gulp-react'),
     rename = require('gulp-rename'),
     jshint = require('gulp-jshint'),
     locate = require('./lib/locate'),
-    jsxs = locate.all(null, 'react', /jsx$/);
+    jsxs = locate.all(null, 'react', /jsx$/),
 
-gulp.task('build_jsx', function() {
-    gulp.src('react/*.jsx', { read: false })
-    .pipe(sourcemaps.init())
-    .pipe(browserify({
+bundleAll = function (b) {
+    b.bundle()
+    .on('error', function (E) {
+        console.log(E);
+    })
+    .pipe(source('static/js/main.js'))
+    .pipe(gulp.dest(''));
+//    .pipe(concat('main.js'))
+//    .pipe(sourcemaps.write())
+//    .pipe(gulp.dest('static/js/'));
+},
+
+buildJsx = function (watch) {
+    var b = browserify({
+        cache: {},
         basedir: __dirname,
         require: jsxs,
+        files: jsxs,
         bundleExternal: false,
-        transform: 'reactify'
-    }))
+        fullPaths: watch,
+        debug: watch
+    });
+
+    b.transform('reactify');
+
+    if (watch) {
+        b = watchify(b);
+        b.on('update', function () {
+            bundleAll(b);
+        });
+    }
+
+    bundleAll(b);
+};
+
+gulp.task('buildJsxJs', function () {
+    gulp.src('react/*.jsx')
+    .pipe(react({sourceMap: true}))
     .pipe(rename({extname: '.js'}))
-    .pipe(gulp.dest('static/jsx/'))
-    .pipe(concat('main.js'))
-    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('static/jsx/'));
+});
+
+gulp.task('build_jsx', function () {
+    buildJsx(false);
+});
+
+gulp.task('watch_jsx', function () {
+    buildJsx(true);
+});
+
+gulp.task('build_react_core', function () {
+    browserify({require: 'react'})
+    .bundle()
+    .pipe(source('node_modules/react/react.js'))
     .pipe(gulp.dest('static/js/'));
 });
 
-gulp.task('build_react_core', function() {
-    gulp.src('node_modules/react/react.js')
-    .pipe(browserify({
-        require: 'react'
-    }))
-    .pipe(gulp.dest('static/js/'));
-});
-
-gulp.task('lint_js', function() {
+gulp.task('lint_js', function () {
     gulp.src(['ajax/*.js', 'data/*.js', 'module/*.js', 'page/*.js'])
     .pipe(jshint());
 });
 
-gulp.task('develop', ['build_jsx', 'watch', 'server']);
+gulp.task('develop', ['watch_jsx', 'server']);
 
-gulp.task('server', function() {
+gulp.task('server', function( ) {
     nodemon({script: 'webtasks.js', ext: 'js'})
     .on('change', ['lint_js']);
 });
 
 gulp.task('buildall', ['build_react_core', 'build_jsx']);
 gulp.task('default',['buildall']);
-
-gulp.task('watch', function() {
-    gulp.watch('react/*.jsx', ['build_jsx']);
-});
--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,4 @@
-require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({"./react/Bug.jsx":[function(require,module,exports){
-module.exports=require('hK0KT0');
-},{}],"hK0KT0":[function(require,module,exports){
+require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({"./react/Bug.jsx":[function(require,module,exports){
 /** @jsx React.DOM */
 var React = require('react'),
     notfound = require('NotFoundModule'),
@@ -15,9 +13,7 @@ Bug = React.createClass({displayName: 'Bug',
 
 module.exports = Bug;
 
-},{}],"./react/LimitSelect.jsx":[function(require,module,exports){
-module.exports=require('W+i0zc');
-},{}],"W+i0zc":[function(require,module,exports){
+},{"NotFoundModule":undefined,"react":undefined}],"./react/LimitSelect.jsx":[function(require,module,exports){
 /** @jsx React.DOM */
 var React = require('react'),
 
@@ -30,7 +26,8 @@ LimitSelect = React.createClass({displayName: 'LimitSelect',
             options.push(
                 React.createElement("option", null, I+1)
             );
-        };
+        }
+
         return (
             React.createElement("select", null, 
              options
@@ -41,7 +38,7 @@ LimitSelect = React.createClass({displayName: 'LimitSelect',
 
 module.exports = LimitSelect;
 
-},{}],"tSjdzg":[function(require,module,exports){
+},{"react":undefined}],"./react/Path.jsx":[function(require,module,exports){
 /** @jsx React.DOM */
 var React = require('react'),
 
@@ -50,7 +47,7 @@ Path = React.createClass({displayName: 'Path',
         var paths = this.props.path.map(function (P) {
             return (
                 React.createElement("li", null, React.createElement("a", {href: '/cate/' + P.id}, P.title))
-            )
+            );
         });
         return (
             React.createElement("ul", {className: "path"}, 
@@ -62,18 +59,14 @@ Path = React.createClass({displayName: 'Path',
 
 module.exports = Path;
 
-},{}],"./react/Path.jsx":[function(require,module,exports){
-module.exports=require('tSjdzg');
-},{}],"./react/Product.jsx":[function(require,module,exports){
-module.exports=require('FNcvG+');
-},{}],"FNcvG+":[function(require,module,exports){
+},{"react":undefined}],"./react/Product.jsx":[function(require,module,exports){
 /** @jsx React.DOM */
 var React = require('react'),
     LimitSelect = require('./LimitSelect.jsx'),
 
 Product = React.createClass({displayName: 'Product',
     getInitialState: function () {
-        return {selectedSpec: this.props.specs[0].id}
+        return {selectedSpec: this.props.specs[0].id};
     },
     handleSpecChange: function () {
         this.setState({selectedSpec: event.target.value});
@@ -107,334 +100,4 @@ React.createElement("div", null,
 
 module.exports = Product;
 
-},{"./LimitSelect.jsx":"W+i0zc"}]},{},["hK0KT0"])
-require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({"./react/Bug.jsx":[function(require,module,exports){
-module.exports=require('hK0KT0');
-},{}],"hK0KT0":[function(require,module,exports){
-/** @jsx React.DOM */
-var React = require('react'),
-    notfound = require('NotFoundModule'),
-
-Bug = React.createClass({displayName: 'Bug',
-    render: function () {
-        return (
-            React.createElement("div", null, "OK!")
-        );
-    }
-});
-
-module.exports = Bug;
-
-},{}],"./react/LimitSelect.jsx":[function(require,module,exports){
-module.exports=require('W+i0zc');
-},{}],"W+i0zc":[function(require,module,exports){
-/** @jsx React.DOM */
-var React = require('react'),
-
-LimitSelect = React.createClass({displayName: 'LimitSelect',
-    render: function () {
-        var options = [],
-            I;
-
-        for (I=0;I<this.props.max;I++) {
-            options.push(
-                React.createElement("option", null, I+1)
-            );
-        };
-        return (
-            React.createElement("select", null, 
-             options
-            )
-        );
-    }
-});
-
-module.exports = LimitSelect;
-
-},{}],"tSjdzg":[function(require,module,exports){
-/** @jsx React.DOM */
-var React = require('react'),
-
-Path = React.createClass({displayName: 'Path',
-    render: function () {
-        var paths = this.props.path.map(function (P) {
-            return (
-                React.createElement("li", null, React.createElement("a", {href: '/cate/' + P.id}, P.title))
-            )
-        });
-        return (
-            React.createElement("ul", {className: "path"}, 
-             paths
-            )
-        );
-    }
-});
-
-module.exports = Path;
-
-},{}],"./react/Path.jsx":[function(require,module,exports){
-module.exports=require('tSjdzg');
-},{}],"./react/Product.jsx":[function(require,module,exports){
-module.exports=require('FNcvG+');
-},{}],"FNcvG+":[function(require,module,exports){
-/** @jsx React.DOM */
-var React = require('react'),
-    LimitSelect = require('./LimitSelect.jsx'),
-
-Product = React.createClass({displayName: 'Product',
-    getInitialState: function () {
-        return {selectedSpec: this.props.specs[0].id}
-    },
-    handleSpecChange: function () {
-        this.setState({selectedSpec: event.target.value});
-    },
-    render: function () {
-        var specs = [],
-            max,
-            I;
-
-        for (I=0;I<this.props.specs.length;I++) {
-            if (this.props.specs[I].id == this.state.selectedSpec) {
-                max = this.props.specs[I].limit;
-            }
-            specs.push(
-                React.createElement("option", {value: this.props.specs[I].id}, this.props.specs[I].title)
-            );
-        }
-
-        return (
-React.createElement("div", null, 
- React.createElement("h1", null, this.props.title), 
- React.createElement("p", null, this.props.description), 
- React.createElement("span", null, "Price: ", React.createElement("i", null, "$"), React.createElement("b", null, this.props.price)), 
- React.createElement("div", null, "Pick a spec:", React.createElement("select", {onChange: this.handleSpecChange, value: this.state.selectedSpec}, specs), 
-  React.createElement(LimitSelect, {max: max})
- )
-)
-        );
-    }
-});
-
-module.exports = Product;
-
-},{"./LimitSelect.jsx":"W+i0zc"}]},{},["W+i0zc"])
-require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({"./react/Bug.jsx":[function(require,module,exports){
-module.exports=require('hK0KT0');
-},{}],"hK0KT0":[function(require,module,exports){
-/** @jsx React.DOM */
-var React = require('react'),
-    notfound = require('NotFoundModule'),
-
-Bug = React.createClass({displayName: 'Bug',
-    render: function () {
-        return (
-            React.createElement("div", null, "OK!")
-        );
-    }
-});
-
-module.exports = Bug;
-
-},{}],"./react/LimitSelect.jsx":[function(require,module,exports){
-module.exports=require('W+i0zc');
-},{}],"W+i0zc":[function(require,module,exports){
-/** @jsx React.DOM */
-var React = require('react'),
-
-LimitSelect = React.createClass({displayName: 'LimitSelect',
-    render: function () {
-        var options = [],
-            I;
-
-        for (I=0;I<this.props.max;I++) {
-            options.push(
-                React.createElement("option", null, I+1)
-            );
-        };
-        return (
-            React.createElement("select", null, 
-             options
-            )
-        );
-    }
-});
-
-module.exports = LimitSelect;
-
-},{}],"tSjdzg":[function(require,module,exports){
-/** @jsx React.DOM */
-var React = require('react'),
-
-Path = React.createClass({displayName: 'Path',
-    render: function () {
-        var paths = this.props.path.map(function (P) {
-            return (
-                React.createElement("li", null, React.createElement("a", {href: '/cate/' + P.id}, P.title))
-            )
-        });
-        return (
-            React.createElement("ul", {className: "path"}, 
-             paths
-            )
-        );
-    }
-});
-
-module.exports = Path;
-
-},{}],"./react/Path.jsx":[function(require,module,exports){
-module.exports=require('tSjdzg');
-},{}],"./react/Product.jsx":[function(require,module,exports){
-module.exports=require('FNcvG+');
-},{}],"FNcvG+":[function(require,module,exports){
-/** @jsx React.DOM */
-var React = require('react'),
-    LimitSelect = require('./LimitSelect.jsx'),
-
-Product = React.createClass({displayName: 'Product',
-    getInitialState: function () {
-        return {selectedSpec: this.props.specs[0].id}
-    },
-    handleSpecChange: function () {
-        this.setState({selectedSpec: event.target.value});
-    },
-    render: function () {
-        var specs = [],
-            max,
-            I;
-
-        for (I=0;I<this.props.specs.length;I++) {
-            if (this.props.specs[I].id == this.state.selectedSpec) {
-                max = this.props.specs[I].limit;
-            }
-            specs.push(
-                React.createElement("option", {value: this.props.specs[I].id}, this.props.specs[I].title)
-            );
-        }
-
-        return (
-React.createElement("div", null, 
- React.createElement("h1", null, this.props.title), 
- React.createElement("p", null, this.props.description), 
- React.createElement("span", null, "Price: ", React.createElement("i", null, "$"), React.createElement("b", null, this.props.price)), 
- React.createElement("div", null, "Pick a spec:", React.createElement("select", {onChange: this.handleSpecChange, value: this.state.selectedSpec}, specs), 
-  React.createElement(LimitSelect, {max: max})
- )
-)
-        );
-    }
-});
-
-module.exports = Product;
-
-},{"./LimitSelect.jsx":"W+i0zc"}]},{},["tSjdzg"])
-require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({"./react/Bug.jsx":[function(require,module,exports){
-module.exports=require('hK0KT0');
-},{}],"hK0KT0":[function(require,module,exports){
-/** @jsx React.DOM */
-var React = require('react'),
-    notfound = require('NotFoundModule'),
-
-Bug = React.createClass({displayName: 'Bug',
-    render: function () {
-        return (
-            React.createElement("div", null, "OK!")
-        );
-    }
-});
-
-module.exports = Bug;
-
-},{}],"./react/LimitSelect.jsx":[function(require,module,exports){
-module.exports=require('W+i0zc');
-},{}],"W+i0zc":[function(require,module,exports){
-/** @jsx React.DOM */
-var React = require('react'),
-
-LimitSelect = React.createClass({displayName: 'LimitSelect',
-    render: function () {
-        var options = [],
-            I;
-
-        for (I=0;I<this.props.max;I++) {
-            options.push(
-                React.createElement("option", null, I+1)
-            );
-        };
-        return (
-            React.createElement("select", null, 
-             options
-            )
-        );
-    }
-});
-
-module.exports = LimitSelect;
-
-},{}],"tSjdzg":[function(require,module,exports){
-/** @jsx React.DOM */
-var React = require('react'),
-
-Path = React.createClass({displayName: 'Path',
-    render: function () {
-        var paths = this.props.path.map(function (P) {
-            return (
-                React.createElement("li", null, React.createElement("a", {href: '/cate/' + P.id}, P.title))
-            )
-        });
-        return (
-            React.createElement("ul", {className: "path"}, 
-             paths
-            )
-        );
-    }
-});
-
-module.exports = Path;
-
-},{}],"./react/Path.jsx":[function(require,module,exports){
-module.exports=require('tSjdzg');
-},{}],"./react/Product.jsx":[function(require,module,exports){
-module.exports=require('FNcvG+');
-},{}],"FNcvG+":[function(require,module,exports){
-/** @jsx React.DOM */
-var React = require('react'),
-    LimitSelect = require('./LimitSelect.jsx'),
-
-Product = React.createClass({displayName: 'Product',
-    getInitialState: function () {
-        return {selectedSpec: this.props.specs[0].id}
-    },
-    handleSpecChange: function () {
-        this.setState({selectedSpec: event.target.value});
-    },
-    render: function () {
-        var specs = [],
-            max,
-            I;
-
-        for (I=0;I<this.props.specs.length;I++) {
-            if (this.props.specs[I].id == this.state.selectedSpec) {
-                max = this.props.specs[I].limit;
-            }
-            specs.push(
-                React.createElement("option", {value: this.props.specs[I].id}, this.props.specs[I].title)
-            );
-        }
-
-        return (
-React.createElement("div", null, 
- React.createElement("h1", null, this.props.title), 
- React.createElement("p", null, this.props.description), 
- React.createElement("span", null, "Price: ", React.createElement("i", null, "$"), React.createElement("b", null, this.props.price)), 
- React.createElement("div", null, "Pick a spec:", React.createElement("select", {onChange: this.handleSpecChange, value: this.state.selectedSpec}, specs), 
-  React.createElement(LimitSelect, {max: max})
- )
-)
-        );
-    }
-});
-
-module.exports = Product;
-
-},{"./LimitSelect.jsx":"W+i0zc"}]},{},["FNcvG+"])
+},{"./LimitSelect.jsx":undefined,"react":undefined}]},{},[]);
