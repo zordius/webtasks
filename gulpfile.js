e28e09f31ac25134ba021f4bc432fad34f392d94
Zordius Chen <zordius@yahoo-inc.com>
2014-11-26 13:42:04 +0800

    update nodemon

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var gulp = require('gulp'),
     watchify = require('watchify'),
     source = require('vinyl-source-stream'),
     concat = require('gulp-concat'),
-    nodemon = require('gulp-nodemon'),
+    nodemon = require('nodemon'),
     readable = require('stream').Readable,
     react = require('gulp-react'),
     jshint = require('gulp-jshint'),
@@ -33,25 +33,27 @@ buildJsx = function (watch) {
     b.require(stream, {expose: '$R'});
 
     if (watch) {
-        b = watchify(b);
+        b = watchify(b, {delay: 1000});
         b.on('update', function () {
+console.log(arguments);
             gutil.log('[browserify] updated~');
             bundleAll(b);
         });
     }
 
     bundleAll(b);
+    return b;
 };
 
 stream.push(code);
 stream.push(null);
 
 gulp.task('build_jsx', function () {
-    buildJsx(false);
+    return buildJsx(false);
 });
 
 gulp.task('watch_jsx', function () {
-    buildJsx(true);
+    return buildJsx(true);
 });
 
 gulp.task('lint_js', function () {
@@ -61,9 +63,15 @@ gulp.task('lint_js', function () {
 
 gulp.task('develop', ['watch_jsx', 'server']);
 
-gulp.task('server', function( ) {
-    nodemon({script: require(process.cwd() + '/package.json').main, ext: 'js'})
-    .on('change', ['lint_js']);
+gulp.task('server', function() {
+    nodemon({
+        watch: 'static/js/main.js',
+        script: require(process.cwd() + '/package.json').main,
+        ext: 'js'
+    })
+    .on('log', function (log) {
+        gutil.log(log.colour);
+    });
 });
 
 gulp.task('buildall', ['build_jsx']);
